72a945b8c773e35e1e1f366fbc0e1ce2086cd5f3
h-edgethedev <oluwanifemibarber@gmail.com>
2026-07-25 22:00:44 +0100

    still learning react

--- Working with Data in React/src/App.jsx
+++ Working with Data in React/src/App.jsx
@@ -1,16 +1,47 @@
-function App(){
-  return(
-    <>
-    <Greeting name= "Heritage" />
-    <Greeting name= "Phillips" />
-    <Greeting name= "Mayowa" />
-    </>
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+      <Button buttonText="Click Me" isActive={true}/>
+    </div>
+  )
+
+}
+
+function UserList() {
+  const users = [
+    { id: "user-001-employee", name: "Alice", email: "alice@example.com" },
+    { id: "user-002-employee", name: "Bob", email: "bob@example.com" },
+    { id: "user-003-employee", name: "John", email: "john@example.com" },
+  ];
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h3>{user.name} </h3>
+          <h3>{user.email} </h3>
+          <h3>{user.id} </h3>
+        </div>
+      ))}
+    </div>
   )
 }
 
-function Greeting(props){
-  console.log(props)
-  return <h1>Hi {props.name}!</h1>
+function Button({ buttonText, isActive }) {
+  const defaultStyles = {
+    backgroundColor: isActive ? "green": "blue",
+    color: "white",
+    border: "none",
+    borderRadius: "4px",
+    padding: "10px 20px",
+    fontSize: "16px",
+    fontWeight: "bold",
+    cursor: "pointer",
+    transition: "background-color 0.3s ease",    
+  };
+
+  return <button style={defaultStyles}>{buttonText}</button>;
 }
 
-export default App
+
+export default App;
