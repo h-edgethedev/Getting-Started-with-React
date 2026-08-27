6d24929daa813042c9aff955510faac9c30af009
h-edgethedev <oluwanifemibarber@gmail.com>
2026-08-27 22:45:17 +0100

    Working on Events in React

--- Building-a-moodboard/src/App.jsx
+++ Building-a-moodboard/src/App.jsx
@@ -1,12 +1,26 @@
 import "./moodboard.jsx"
 import './App.css'
 import { MoodBoard, MoodBoardItem } from "./moodboard.jsx"
+import { useState } from "react"
 
-function App(){
-  return(
-  <>
-    <MoodBoard/>
-  </>
+function handleSubmit(event) {
+  event.preventDefault()
+
+  const text = document.getElementById("Text")
+
+  console.log("Form Submitted!")
+  console.log(text.value)
+}
+
+function App() {
+  return (
+    <>
+      <MoodBoard />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="Text" id="Text" placeholder="Input name" />
+        <button>Submit</button>
+      </form>
+    </>
   )
 }
 
