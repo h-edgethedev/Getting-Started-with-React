71097f67e75bf19c6e11e57f2a287b31d13fb729
h-edgethedev <oluwanifemibarber@gmail.com>
2026-08-30 21:56:54 +0100

    Going to sleep. Continuing tomorrow morning

--- Task-manager/src/TaskTracker.jsx
+++ Task-manager/src/TaskTracker.jsx
@@ -3,23 +3,39 @@ import { useState } from "react";
 function TaskTracker() {
     const [input, setInput] = useState("")
     const [tasks, setTask] = useState([])
-    function handleTaskInput(event){
+    function handleTaskInput(event) {
         setInput(event.target.value)
     }
-    function addTask(){
+    function addTask() {
         const newTask = {
-            id: tasks.length+1,
-            task: input
+            id: Date.now(),
+            task: input,
+            date: new Date().toLocaleDateString()
         }
-        setTask((prevTasks)=> [...prevTasks, newTask])
+        setTask((prevTasks) => [...prevTasks, newTask])
         console.log(tasks)
     }
+    function handleDelete(id){
+
+    }
+    // console.log(new Date().toLocaleDateString())
     return (
         <div id="task-div">
-            <input type="text" id="add-task" placeholder="Input Task" onChange={handleTaskInput} value={input}/>
+            <input type="text" id="add-task" placeholder="Input Task" onChange={handleTaskInput} value={input} />
             <button id="add-task-btn" onClick={addTask}>Add</button>
+            <ul>
+                {
+                    tasks.map((task) => (
+                        <li className="task" id="{task.id}">
+                            <strong>{task.task}</strong><br />
+                            <small>{task.date} </small>
+                            <button className="delete-task">Delete</button>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
 
-export default TaskTracker; 
+export default TaskTracker;
