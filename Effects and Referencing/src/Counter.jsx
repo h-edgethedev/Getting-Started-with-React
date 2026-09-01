78d25ebb537a98db9061512e5a632e4fb7c77dc4
Barber Heritage <barberheritage@ISLs-Mac-mini.local>
2026-09-01 13:45:06 +0100

    Pushing some files from work mac

---
+++ Effects and Referencing/src/Counter.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react"
+
+const Counter = ()=>{
+  const [count, setCount] = useState(0)
+  useEffect(()=>{
+    console.log("Component Renders")
+  }, [count])
+
+  return(
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={()=>setCount(count+1)}>Increase</button>
+        <button onClick={()=>setCount(count-1)}>Decrease</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter;
