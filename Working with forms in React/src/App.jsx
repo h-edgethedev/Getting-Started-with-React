3a904540e72522afa19f89b8350fdf1f363ca8db
Heritage Oluwanifemi Barber <oluwanifemibarber@gmail.com>
2026-09-03 16:30:46 +0100

    done for the day

--- Working with forms in React/src/App.jsx
+++ Working with forms in React/src/App.jsx
@@ -1,71 +1,13 @@
-// "use client";
+import "./App.css"
+import { useState, useEffect, useRef } from "react";
+import { OTPGenerator } from "./OTP generator"
 
-// import { useActionState } from "react";
-// import { submitForm } from "./server";
-
-// const App = () => {
-//   const [state, submit, isPending] = useActionState(submitForm, { message: "" })
-//   return (
-//     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
-//       <form action={submit} className="bg-white p-6 rounded-2xl shadow-md w-full max-w-md">
-//         <h2 className="text-2xl text-center font-semibold text-gray-700 mb-4">
-//           Greet Someone
-//         </h2>
-
-//         <input
-//           type="text"
-//           name="name"
-//           placeholder="Enter your name"
-//           required
-//           className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
-//         />
-
-//         <button
-//           type="submit"
-//           disabled={isPending}
-//           className="w-full mt-4 p-3 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-600 disabled:bg-gray-400 transition-all"
-//         >
-//           {isPending ? "Greeting..." : "Greet"}
-//         </button>
-
-//         {state.message && (
-//           <p className="mt-4 text-green-600 text-center font-medium">
-//             {state.message}
-//           </p>
-//         )}
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default App;
-
-"use client";
-
-import { startTransition, useActionState } from "react";
-import { getUsers } from "./GetUsers";
-
-export default function App() {
-  const [users, fetchAction, isPending] = useActionState(getUsers, []);
-
-  return (
-    <div className="p-6 max-w-lg mx-auto">
-      <button
-      onClick={()=> startTransition(()=> fetchAction())}
-        disabled={isPending}
-        className="px-4 py-2 cursor-pointer bg-green-500 text-white rounded-lg hover:bg-green-600 disabled:bg-gray-400 font-bold"
-      >
-        {isPending ? "Fetching Users..." : "Fetch Users"}
-      </button>
-
-      <ul className="mt-4 space-y-2">
-        {users.map((user) => (
-          <li key={user.id} className="p-3 bg-gray-100 rounded-lg">
-            <p className="font-semibold">{user.name}</p>
-            <p className="text-sm text-gray-600">{user.email}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+function App(){
+    return(
+        <>
+            <OTPGenerator/>
+        </>
+    )
 }
+
+export default App;
--- Working with forms in React/src/OTP generator.jsx
+++ Working with forms in React/src/OTP generator.jsx
@@ -1,11 +1,39 @@
-const { useState, useEffect, useRef } = React;
+import { useState, useEffect, useRef } from "react"
 
 export const OTPGenerator = () => {
+    const [otp, setOtp] = useState("null")
+    const intervalId = useRef(null)
+    const [secondsLeft, setSecondsLeft] = useState(null)
+    const [isCounting, setIsCounting] = useState(false)
+    const [hasGenerated, setHasGenerated] = useState(false)
+    function generateOTP(){
+        return Math.floor(Math.random()*1000000)
+    }
+    let timer;
+    function buttonHandler(){
+        setOtp(generateOTP());
+        setSecondsLeft(5)
+        setHasGenerated(true)
+        setIsCounting(true)
+        intervalId.current = setInterval(()=>{
+            setSecondsLeft(prevSeconds => prevSeconds-1)
+        }, 1000)
+    }
+    useEffect(()=>{
+        if(secondsLeft===0){
+            clearInterval(intervalId.current)
+            setIsCounting(false)
+        }
+    }, [secondsLeft])
+    useEffect(()=>{
+        clearInterval(intervalId.current)
+    }, [])
     return(
         <div className="container">
             <h1 id="otp-title">OTP Generator</h1>
-            <h2 id="otp-display">Click "Generate OTP" to get a code</h2>
-            <p id="otp-timer" aria-live="assertive"></p>
-        </div>
+            <h2 id="otp-display">{hasGenerated ? `${otp}`: `Click "Generate OTP" to get a code`}</h2>
+            <p id="otp-timer" aria-live="assertive">{isCounting ? `Expires in: ${secondsLeft} seconds`: hasGenerated ? "OTP expired. Click the button to generate a new OTP.": ""}</p>
+            <button id="generate-otp-button" onClick={buttonHandler} disabled={isCounting}>Generate OTP</button>
+        </div>   
     )
 }
