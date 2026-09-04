625679b76e0a0026a5d580470d1d5ea412ed90a0
Heritage Oluwanifemi Barber <oluwanifemibarber@gmail.com>
2026-09-04 15:50:12 +0100

    done for the day

---
+++ Working with forms in React/Building a Superhero application Form/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { SuperheroForm } from './Superhero'
+
+function App() {
+
+  return (
+    <>
+      <SuperheroForm/>
+    </>
+  )
+}
+
+export default App;
