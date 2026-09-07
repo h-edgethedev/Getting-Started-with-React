63a85ef1fa40e8ce1b03403eb77e63b709627682
h-edgethedev <oluwanifemibarber@gmail.com>
2026-09-07 23:20:35 +0100

    kicking off today's work

---
+++ Memoization in React/src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import ExpensiveSquare from "./hooks/memo";
+
+function App(){
+    return(
+        <ExpensiveSquare/>
+    )
+}
---
+++ Memoization in React/src/hooks/memo.jsx
@@ -0,0 +1,14 @@
+function ExpensiveSquare({ num }) {
+  function calculateSquare(n) {
+    console.log("Calculating square...");
+    return n * n;
+  }
+
+  const squared = calculateSquare(num);
+  return (
+    <p>
+      Square of {num}: {squared}
+    </p>
+  );
+}
+export default ExpensiveSquare;
