45a6e067e0d60e182cdac4314f1ba4b3ead52b5c
h-edgethedev <oluwanifemibarber@gmail.com>
2026-09-07 23:40:51 +0100

    going to bed

--- Memoization in React/src/App.jsx
+++ Memoization in React/src/App.jsx
@@ -1,8 +1,22 @@
 import { useState } from "react";
 import ExpensiveSquare from "./hooks/memo";
+import { useEffect } from "react";
 
 function App(){
+    const [timer, setTimer] = useState(0)
+    const [num, setNum] = useState(0)
+    useEffect(()=>{
+        const interval = setInterval(()=> setTimer((c)=> c+1), 1000)
+        return ()=> clearInterval(interval)
+    }, [])
     return(
-        <ExpensiveSquare/>
+        <div>
+            <h1>Timer: {timer} </h1>
+            <ExpensiveSquare num={num}/>
+            <button onClick={()=> setNum((n)=>n+1)}></button>
+
+        </div>
     )
 }
+
+export default App
--- Memoization in React/src/hooks/memo.jsx
+++ Memoization in React/src/hooks/memo.jsx
@@ -1,14 +1,14 @@
 function ExpensiveSquare({ num }) {
   function calculateSquare(n) {
-    console.log("Calculating square...");
-    return n * n;
+    console.log("calculating square...")
+    return n*n
   }
-
-  const squared = calculateSquare(num);
-  return (
+  const squared = calculateSquare(num)
+  return(
     <p>
       Square of {num}: {squared}
     </p>
-  );
+  )
 }
+
 export default ExpensiveSquare;
