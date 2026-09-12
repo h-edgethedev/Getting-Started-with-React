b112c5fe58c812c4d58077c42f0d76d8bb464807
h-edgethedev <oluwanifemibarber@gmail.com>
2026-09-12 17:32:16 +0100

    I;ll continue later

--- Memoization in React/Building a shopping List App/src/Shop List.jsx
+++ Memoization in React/Building a shopping List App/src/Shop List.jsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
 import { useCallback } from "react";
+import "./App.css"
+const items = ["Apples", "Bananas", "Strawberries", "Blueberries", 'Mangoes', "Pineapples", "Lettuce", "Broccoli", "Paper Towels", "Dish Soap"]
 
-export const ShoppingList= ()=>{
-    return(
+export const ShoppingList = () => {
+    const [query, setQuery] = useState("")
+    const [selectedItems, setSelectedItems] = useState([])
+    const filteredItems = items.filter((item) => item.toLowerCase().includes(query.toLowerCase()))
+    function toggleItem(item) {
+        setSelectedItems
+    }
+    return (
         <div className="container">
             <h1>Shopping List</h1>
             <form>
                 <label htmlFor="search">Search for an item:
-                    <input type="search" name="" id="" />
+                    <input type="search" name="search" id="search" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Search..." aria-describedby="search-description" />
                 </label>
+                <p id="search-description">Type to filter the list below:</p>
+                <ul>
+                    {filteredItems.map((item) => (
+
+                        <li key={item}><label><input type="checkbox" onChange={(item) => toggleItem(item)} />{item}</label></li>
+                    ))}
+                </ul>
             </form>
         </div>
     )
